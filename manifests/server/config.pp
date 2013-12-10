b89f06c81a01683f267c567332c4145636d7d03f
Arnaud Ragonnet <Arnaud.Ragonnet@francetv.fr>
2013-12-10 12:54:37 +0100

    info

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -23,8 +23,10 @@ class mysql::server::config {
   }
 
   if $mysql::server::manage_config_file  {
+    info 'Gestion du fichier de configuration Mysql'
     if has_key($options['mysqld'], 'innodb_log_file_size') {
-      file { "${options['mysqld']['datadir']}/old_innodb_log_file_size":
+      info 'Modifications enventuell des logs INNODB'
+      file { "${options['mysqld']['datadir']}/old_innodb_log_file":
         ensure  => directory,
         mode    => '0755',
         owner   => 'mysql',
@@ -33,9 +35,9 @@ class mysql::server::config {
 
       augeas{ 'backup_old_innodb_log_file_size':
         context => $mysql::server::config_file,
-        changes => "mv ${options['mysqld']['datadir']}/ib_logfile* ${options['mysqld']['datadir']}/old_innodb_log_file_size/",
+        changes => "mv ${options['mysqld']['datadir']}/ib_logfile* ${options['mysqld']['datadir']}/old_innodb_log_file/",
         onlyif  => "match mysqld/innodb_log_file_size != ${options['mysqld']['innodb_log_file_size']}",
-        require => File["${options['mysqld']['datadir']}/old_innodb_log_file_size"],
+        require => File["${options['mysqld']['datadir']}/old_innodb_log_file"],
         before  => File[$mysql::server::config_file],
       }
     }
